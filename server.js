2c88d67aeb10f2265bc7a41afc1d929371089ee8
Valentina <Valentina.bennett@hmcts.net>
2021-03-21 13:36:14 +0000

    add favicon and fix for heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const favicon = require("serve-favicon");
 
 const app = express();
 
 const people = require("./routes/people");
-
+app.use(favicon(path.join(__dirname, "Dist/favicon.ico")));
 app.use(express.static(path.join(__dirname, "Dist")));
 app.use("/api/people", people);
 
