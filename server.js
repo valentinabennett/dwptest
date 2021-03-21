1261cd37fee987702198e0c1904cdef3c8641122
Valentina <Valentina.bennett@hmcts.net>
2021-03-21 14:01:54 +0000

    fix heroku

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const favicon = require("serve-favicon");
 const app = express();
 
 const people = require("./routes/people");
+
+const port = process.env.PORT || 3800;
 app.use(favicon(path.join(__dirname, "Dist/favicon.ico")));
 app.use(express.static(path.join(__dirname, "Dist")));
 app.use("/api/people", people);
 
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "Dist/index.html"));
 });
 
-app.set("port", process.env.PORT || 3800);
-
-app.listen(process.env.PORT || 3800);
-console.log("Listening on port: " + app.get("port"));
+app.listen(port);
+console.log("Listening on port: " + port);
 
 module.exports = app;
